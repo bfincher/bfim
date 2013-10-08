e10338bbe0ef842374fe80986fd3f5047baf45a3
Brian Fincher <brian@fincherhome.com>
2013-10-08 18:43:31 -0500

    adjust urls

--- python/bfim/urls.py
+++ python/bfim/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('bfim.views',
-    url(r'^bfim/$', 'index'),
+    url(r'^$', 'index'),
     url(r'content/.*', 'content'),
-    url(r'^$', 'content')
     )
